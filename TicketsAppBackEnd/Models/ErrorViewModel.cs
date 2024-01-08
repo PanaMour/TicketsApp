c420e78f6f78b8dae36d1a63d35d0a755fa323a0
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2024-01-08 21:44:37 +0200

    backend

---
+++ TicketsAppBackEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TicketsAppBackEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
