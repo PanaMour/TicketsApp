9e52e7a86a1e45d8805a0230f10a5dc1eb339760
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2024-04-05 16:38:28 +0300

    Update EmailService.cs

--- TicketsApp/Models/EmailService.cs
+++ TicketsApp/Models/EmailService.cs
@@ -8,16 +8,16 @@
     {
         public async Task SendEmailAsync(string to, string subject, string htmlMessage)
         {
-            var smtpClient = new SmtpClient("smtp.example.com")
+            var smtpClient = new SmtpClient("smtp.google.com")
             {
                 Port = 587,
-                Credentials = new NetworkCredential("username@example.com", "password"),
+                Credentials = new NetworkCredential("ticketsappnoreply@gmail.com", "ticketsappnoreply"),
                 EnableSsl = true,
             };
 
             var mailMessage = new MailMessage
             {
-                From = new MailAddress("noreply@example.com"),
+                From = new MailAddress("ticketsappnoreply@google.com"),
                 Subject = subject,
                 Body = htmlMessage,
                 IsBodyHtml = true,
